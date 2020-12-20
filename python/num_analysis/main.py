45bb7afbc28b4fddb9cd4a08a448910fa7c6b4a1
BezSovesty <wa.drozdov@gmail.com>
2020-12-20 23:33:32 +0300

    Добавлено чтение из файла.

--- python/num_analysis/main.py
+++ python/num_analysis/main.py
@@ -1,3 +1,11 @@
+def lab2():
+    # Чтение
+    data = []
+    with open('./data/matrix.txt', 'r') as file:
+        for line in file:
+            data.append([float(x) for x in line.split()])
+
+
 if __name__ == '__main__':
     try:
         print('Какую лабораторную запустить?\nВарианты (2): ')
